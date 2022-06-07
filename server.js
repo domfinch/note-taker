86c409e9c1b68f9c8bbd45885c242010e00ec9a3
domfinch <finch.163@buckeyemail.osu.edu>
2022-06-07 16:13:41 -0400

    linking updated

--- server.js
+++ server.js
@@ -9,3 +9,24 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.get('/api/notes', (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes.slice(1))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.post("/api/notes", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log(newNote);
+    res.json(savedNotes);
+})
+
