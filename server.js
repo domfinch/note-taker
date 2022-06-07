379ee08ce56d0d43e8e82725e176c644efa3d1d3
domfinch <finch.163@buckeyemail.osu.edu>
2022-06-07 16:03:16 -0400

    linking

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4001;
+const fs = require('fs');
+const path = require('path');
+const allNotes = require('./db/db.json');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
